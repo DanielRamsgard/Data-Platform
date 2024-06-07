8fb6978693d136ab3f5261b07531ea96811711f5
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-06-06 20:18:55 -0400

    window reload

--- data-platform-react/src/App.js
+++ data-platform-react/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import TerminalView from './components/TerminalView';
 import WideView from './components/WideView';
 import ContactView from './components/ContactView';
+import { HandleTerminalClick } from './components/HandleTerminalClick';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
         <div className="Container">
             <div className="Header">
                 <div className="Nav">
-                    <button className="Button"><img src="/static/media/terminal.png" className="Terminal"></img></button>
+                    <button className="Button" onClick={HandleTerminalClick}><img src="/static/media/terminal.png" className="Terminal"></img></button>
                     <button className="Button"> Gaussian Elim </button>
                     <button className="Button"> Eigenvalues </button>
                     <button className="Button"> SVD </button>
